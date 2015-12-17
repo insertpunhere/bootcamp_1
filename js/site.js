0545455fd7e91b1e86c2bc517b3b80fca36a7bbe
anaklusmos <ejames1998@gmail.com>
2015-12-17 03:31:10 +0000

    We finished our home page

---
+++ js/site.js
@@ -0,0 +1,105 @@
+// html5media enables <video> and <audio> tags in all major browsers
+// External File: http://api.html5media.info/1.1.8/html5media.min.js
+
+// Add user agent as an attribute on the <html> tag...
+// Inspiration: http://css-tricks.com/ie-10-specific-styles/
+var b = document.documentElement;
+b.setAttribute('data-useragent',  navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform );
+
+
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+    var supportsAudio = !! document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false;
+        mediaPath = '/anaklusmos/bootcamp_1/media/',
+        extension = '',
+        tracks = [{
+            "track": 1,
+            "name": "Odor to Joy-Exits Exist",
+            "length": "03:45",
+            "file": "odor"
+        }, {
+            "track": 2,
+            "name": "Sweet Nectar-Exits Exist",
+            "length": "02:47",
+            "file": "nectar"
+        }, {
+            "track": 3,
+            "name": "Blain's Song-Exits Exist",
+            "length": "04:41",
+            "file": "blains"
+        }],
+        trackCount = tracks.length,
+        npAction = $('#npAction'),
+        npTitle = $('#npTitle'),
+        audio = $('#audio1').bind('play', function () {
+            playing = true;
+            npAction.text('Now Playing...');
+        }).bind('pause', function () {
+            playing = false;
+            npAction.text('Paused...');
+        }).bind('ended', function () {
+            npAction.text('Paused...');
+            if ((index + 1) < trackCount) {
+                index++;
+                loadTrack(index);
+                audio.play();
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }).get(0),
+        btnPrev = $('#btnPrev').click(function () {
+            if ((index - 1) > -1) {
+                index--;
+                loadTrack(index);
+                if (playing) {
+                    audio.play();
+                }
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }),
+        btnNext = $('#btnNext').click(function () {
+            if ((index + 1) < trackCount) {
+                index++;
+                loadTrack(index);
+                if (playing) {
+                    audio.play();
+                }
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }),
+        li = $('#plList li').click(function () {
+            var id = parseInt($(this).index());
+            if (id !== index) {
+                playTrack(id);
+            }
+        }),
+        loadTrack = function (id) {
+            $('.plSel').removeClass('plSel');
+            $('#plList li:eq(' + id + ')').addClass('plSel');
+            npTitle.text(tracks[id].name);
+            index = id;
+            audio.src = mediaPath + tracks[id].file + extension;
+        },
+        playTrack = function (id) {
+            loadTrack(id);
+            audio.play();
+        };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : audio.canPlayType('audio/mpeg') ? '.m4a':audio.canPlayType('audio/wave') ? '.wav': '';
+        loadTrack(index);
+    }
+});
